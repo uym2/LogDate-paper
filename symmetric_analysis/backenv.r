20701a7ffc0a45e468642433db356a790e858056
Siavash Mirarab <smirarab@gmail.com>
2020-07-24 09:40:15 -0700

    update

--- symmetric_analysis/backenv.r
+++ symmetric_analysis/backenv.r
@@ -1,6 +1,9 @@
 require(ggplot2)
+require(reshape2)
+require(scales)
 
-sc = 100000
+
+sc = 500000
 
 b=0.1
 s=200
@@ -8,7 +11,7 @@ s=200
 
 ###################### Product ################
 
-be = rnorm(n=sc,b,sqrt(b/s))
+be = vapply(rnorm(n=sc,b,sqrt(b/s)),FUN=function(x) max(x,0.000001),1)
 
 lsd = function(be,b,r) {be=be; r = r; b/(be*r)-1}
 logD = function(be,b,r) {be=be; r =r; log(b/(be*r))}
@@ -23,49 +26,86 @@ summary(rCN(1/2)); var(rCN(1/2));
 summary(rLN(1/2)); var(rLN(1/2));
 summary(rg(1/2)); var(rg(1/2))
 
+rgl2 = rg(1/9); rg13 = rg(1/3);      rgm = rg(1);     #rgn = rg(1/1000);
+rLNl2 = rLN(1/9); rLN13 = rLN(1/3);  rLNm = rLN(1);  #rlNn = rLN(1/1000);
+rCNl2 = rCN(1/9); rCN13 = rCN(1/3);  rCNm = rCN(1); #rCNn = rCN(1/1000);
+rex = rexp();
+
 d=rbind(
   #data.frame(m="LSD",r="Strict",n=lsd(be,b,1),v=0),
-  #data.frame(m="LogD",r="Strict",n=logD(be,b,1),v=0),
-  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rg(1/log(2)^2)),v="Var: 2.1"),
-  data.frame(m="LogD",r="Gamma",n=logD(be,b,rg(1/log(2)^2)),v="Var: 2.1"),
-  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rg(1/3)),v="Var: 0.33"),
-  data.frame(m="LogD",r="Gamma",n=logD(be,b,rg(1/3)),v="Var: 0.33"),
-  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rg(1/1000)),v="Strict"),
-  data.frame(m="LogD",r="Gamma",n=logD(be,b,rg(1/1000)),v="Strict"),
-  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLN(1/1000)),v="Strict"),
-  data.frame(m="LogD",r="LogNormal",n=logD(be,b,rLN(1/1000)),v="Strict"),
-  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLN(1/log(2)^2)),v="Var: 2.1"),
-  data.frame(m="LogD",r="LogNormal",n=logD(be,b,rLN(1/log(2)^2)),v="Var: 2.1"),                
-  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLN(1/3)),v="Var: 0.33"),
-  data.frame(m="LogD",r="LogNormal",n=logD(be,b,rLN(1/3)),v="Var: 0.33"),
-  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCN(1/log(2)^2)),v="Var: 2.1"),
-  data.frame(m="LogD",r="LogUnif",n=logD(be,b,rCN(1/log(2)^2)),v="Var: 2.1"),  
-  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCN(1/3)),v="Var: 0.33"),
-  data.frame(m="LogD",r="LogUnif",n=logD(be,b,rCN(1/3)),v="Var: 0.33"),
-  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCN(1/1000)),v="Strict"),
-  data.frame(m="LogD",r="LogUnif",n=logD(be,b,rCN(1/1000)),v="Strict") , 
-  data.frame(m="LSD",r="Exponential",n=lsd(be,b,rexp()),v="Var: 2.1"),
-  data.frame(m="LogD",r="Exponential",n=logD(be,b,rexp()),v="Var: 2.1")
-)
+  #data.frame(m="LogDate",r="Strict",n=logD(be,b,1),v=0),
+  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rgl2),v="Var: 1/9",o=be*rgl2),
+  data.frame(m="LogDate",r="Gamma",n=logD(be,b,rgl2),v="Var: 1/9",o=be*rgl2),
+  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLNl2),v="Var: 1/9",o=be*rLNl2),
+  data.frame(m="LogDate",r="LogNormal",n=logD(be,b,rLNl2),v="Var: 1/9",o=be*rLNl2),   
+  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCNl2),v="Var: 1/9",o=be*rCNl2),
+  data.frame(m="LogDate",r="LogUnif",n=logD(be,b,rCNl2),v="Var: 1/9",o=be*rCNl2), 
+  
+  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rg13),v="Var:1/3",o=be*rg13),
+  data.frame(m="LogDate",r="Gamma",n=logD(be,b,rg13),v="Var:1/3",o=be*rg13),
+  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLN13),v="Var:1/3",o=be*rLN13),
+  data.frame(m="LogDate",r="LogNormal",n=logD(be,b,rLN13),v="Var:1/3",o=be*rLN13),
+  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCN13),v="Var:1/3",o=be*rCN13),
+  data.frame(m="LogDate",r="LogUnif",n=logD(be,b,rCN13),v="Var:1/3",o=be*rCN13),
+  
+  
+  data.frame(m="LSD",r="Gamma",n=lsd(be,b,rgm),v="Var: 1",o=be*rgm),
+  data.frame(m="LogDate",r="Gamma",n=logD(be,b,rgm),v="Var: 1",o=be*rgm),
+  data.frame(m="LSD",r="LogNormal",n=lsd(be,b,rLNm),v="Var: 1",o=be*rLNm),
+  data.frame(m="LogDate",r="LogNormal",n=logD(be,b,rLNm),v="Var: 1",o=be*rLNm),
+  data.frame(m="LSD",r="LogUnif",n=lsd(be,b,rCNm),v="Var: 1",o=be*rCNm),
+  data.frame(m="LogDate",r="LogUnif",n=logD(be,b,rCNm),v="Var: 1",o=be*rCNm),
+  
+  data.frame(m="LSD",r="Exponential",n=lsd(be,b,rex),v="Var: 2.1",o=be*rex),
+  data.frame(m="LogDate",r="Exponential",n=logD(be,b,rex),v="Var: 2.1",o=be*rex),
+  
+  data.frame(m="LSD",r="Strict",n=lsd(be,b,1),v="Var: 0",o=be*1),
+  data.frame(m="LogDate",r="Strict",n=logD(be,b,1),v="Var: 0",o=be*1) 
+);
+
+d$n2=d$n^2
+
 ggplot(aes(x=n), data=d)+
-      geom_density(aes(color=m))+geom_histogram(aes(fill=m,y=..density..),position = "identity",binwidth = 0.05,alpha=0.4)+theme_classic()+facet_grid(v~r,scales="free")+ 
-  scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+xlab("Penalty")+xlim(-10,10)+
-  geom_vline(xintercept = 0,linetype=3)+theme(legend.position = c(0.87,0.2))+coord_cartesian(xlim=c(-4,4))
-ggsave("product.pdf",width=10,height = 6)
+      geom_density(aes(color=m))+geom_histogram(aes(fill=m,y=..density..),position = "identity",binwidth = 0.05,alpha=0.4)+
+  theme_classic()+facet_wrap(~interaction(r,v,sep=": "),scales="free",ncol=3)+ 
+  scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+xlab("Penalty (without square)")+xlim(-10,10)+
+  geom_vline(xintercept = 0,linetype=3)+theme(legend.position = c(0.87,0.2))+coord_cartesian(xlim=c(-3.3,3.3))
+ggsave("product.pdf",width=7,height = 9)
 
 
-ggplot(aes(x=n,fill=m), data=d)+
-  geom_histogram(position = "identity",binwidth = 0.02,alpha=0.5)+theme_classic()+facet_grid(v~r,scales="free")+ 
-  scale_fill_brewer(name="",palette = "Dark2")+xlab("Penalty")+xlim(c(-10,10))+
-  geom_vline(xintercept = 0,linetype=3)+theme(legend.position = c(0.87,0.2))+coord_cartesian(xlim=c(-1,1))
-ggsave("hist.pdf",width=10,height = 6)
+# ggplot(aes(x=n^2), data=d[d$m=="LSD",])+
+#   #geom_density(aes(color=m),kernel="b",adjust=2)+
+#   geom_histogram(aes(fill=m),position = "identity",binwidth = 0.01,alpha=0.4)+
+#   theme_classic()+facet_wrap(v~r,scales="free")+ 
+#   scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+
+#   xlab("Penalty")+xlim(0,100)+coord_cartesian(xlim=c(0,1))+#scale_x_log10(lim=c(0.0000001,1000))
+#   geom_vline(xintercept = 0,linetype=3)+theme(legend.position = c(0.87,0.2))
+# ggsave("product-square.pdf",width=10,height = 6)
+
+
+d$oc= cut(log(d$o), (-2500:200)/50)
+a = recast(oc+m+r+v~.,data=d,measure.var ="n",fun.aggregate=length)
+a$n2=recast(oc+m+r+v~.,data=d,measure.var ="n2",fun.aggregate=median)$.
+a$n=recast(oc+m+r+v~.,data=d,measure.var ="n",fun.aggregate=median)$.
+a$c = a$.
+
+ggplot(aes(y=c/sc,x=n2,color=m), data=a)+
+  #geom_density(aes(color=m),kernel="b",adjust=2)+
+  geom_point(alpha=0.6)+
+  theme_classic()+theme(legend.position = c(0.8,0.15))+
+  facet_wrap(~interaction(r,v,sep=": "),scales="free",ncol=3)+ 
+  scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+
+  xlab("Penalty")+scale_x_sqrt(lim=c(0,10))+scale_y_continuous(name="Frequency of data",labels=percent)
+ggsave("product-penalty.pdf",width=7,height = 9)
 
 ######################### Compound
 
 
-bg = function(v) {bt = rgamma(sc,1/v,1/v)*b; rnorm(n=5*sc,bt,sqrt(bt/s))}
-gexp = function() {bt = rexp(sc,log(2))*b ; rnorm(n=5*sc,bt,sqrt(bt/s))}
-bLN = function(v) {bt = rlnorm(sc,meanlog = 0,sdlog =sqrt(log(1/2 + 1/2 *sqrt(1 + 4 *v))))*b; rnorm(n=5*sc,bt,sqrt(bt/s))}
+ee  = function(bt) rnorm(n=5*sc,bt,sqrt(bt/s))
+bg = function(v) { ee( rgamma(sc,1/v,1/v)*b)}
+gexp = function() {ee (rexp(sc,log(2))*b )}
+bLN = function(v) {ee (rlnorm(sc,meanlog = 0,sdlog =sqrt(log(1/2 + 1/2 *sqrt(1 + 4 *v))))*b)}
+
 bCN = function(v,a=0.1) {
   x=(1:60)/40;
   p=x[which.min(abs(c((exp(2*x)-exp(-2*x))/4/x-(exp(x)-exp(-x))^2/4/x^2)-v))];
