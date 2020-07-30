c06ab230432a9f22cf1cdb874e26597889d95624
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2020-07-29 18:14:53 -0700

    new figures penalty_llh.pdf and penalty_llh_normal.pdf

--- symmetric_analysis/trueTime_estLen.R
+++ symmetric_analysis/trueTime_estLen.R
@@ -1,8 +1,6 @@
 # phyML estimated
-
 d = read.table("trueTime_estLen.txt",header=T)
 d$clock = factor(d$clock,levels = c("Strict","Lognormal","Gamma","Exponential"))
-
 
 require(ggplot2)
 
@@ -20,6 +18,36 @@ ggplot(d[d$estLen > 1e-3,]) +
   theme(legend.position = "None")
 
 ggsave("trueTime_estLen.pdf",width=4,height=4)
+
+require(reshape2)
+
+d = d[d$estLen > 1e-3,]
+
+d$LogDate = log(d$mu*d$trueTime/d$estLen)
+d$LSD = (d$mu*d$trueTime/d$estLen-1)
+
+d$oc  = cut(d$LogDate,1000);
+d = melt(d,id.vars = c(1:7,10),value.name = "n",variable.name = "m");
+d$n2=d$n^2;
+
+a = recast(oc+m+clock~.,data=d,measure.var ="n",fun.aggregate=length)
+a$n2=recast(oc+m+clock~.,data=d,measure.var ="n2",fun.aggregate=median)$.
+a$n=recast(oc+m+clock~.,data=d,measure.var ="n",fun.aggregate=median)$.
+a$c = a$.
+
+a$m = factor(a$m,levels = c("LSD","LogDate"))
+
+ggplot(aes(x=log(c/t),y=n2,color=m), data=a)+
+  #geom_density(aes(color=m),kernel="b",adjust=2)+
+  #geom_line(aes(group=interaction(m,n>0)),size=0.4)+
+  geom_point(alpha=0.5,size=0.5)+
+  theme_classic()+
+  theme(legend.position = "None")+
+  facet_wrap(~clock,scales="free")+ 
+  scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+
+  scale_y_log10(name="Penalty")+scale_x_continuous(name="Log likelihood (empirical)")
+ggsave("penalty_llh.pdf",width=4,height=4)
+
 
 ###################################################
 
@@ -49,7 +77,7 @@ bs=ee(1*b)
 lsd = function(be,b) {b/be-1}
 logD = function(be,b) {log(b/be)}
 
-d1=rbind(
+d=rbind(
   data.frame(m="LSD",r="Strict",n=lsd(bs,b),v="Var: 0",o=bs),
   data.frame(m="LogDate",r="Strict",n=logD(bs,b),v="Var: 0",o=bs),
   data.frame(m="LSD",r="LogNormal",n=lsd(bLNl2,b),v="Var: 1/6",o=bLNl2),
@@ -60,7 +88,7 @@ d1=rbind(
   data.frame(m="LogDate",r="Exponential",n=logD(bex,b),v="Var: 1",o=bex)
 );
 
-ggplot(aes(x=n), data=d1)+
+ggplot(aes(x=n), data=d)+
   geom_density(aes(color=m))+
   geom_histogram(aes(fill=m,y=..density..),position = "identity",binwidth = 0.05,alpha=0.4)+
   theme_classic()+facet_wrap(~r,scales="free")+ 
@@ -71,4 +99,27 @@ ggplot(aes(x=n), data=d1)+
         legend.key.size = unit(0.5, "lines"))+
   coord_cartesian(xlim=c(-3.3,3.3))
 ggsave("compound_4panels.pdf",width=4,height = 4)
+
+require(reshape2)
+
+dc = dcast(r+v+o~m,data=d,value.var = "n");
+dc$oc  = cut(dc$LogDate,1000);
+d = melt(dc,id.vars = c(1:3,6),value.name = "n",variable.name = "m");
+d$n2=d$n^2;
+
+a = recast(oc+m+r+v~.,data=d,measure.var ="n",fun.aggregate=length)
+a$n2=recast(oc+m+r+v~.,data=d,measure.var ="n2",fun.aggregate=median)$.
+a$n=recast(oc+m+r+v~.,data=d,measure.var ="n",fun.aggregate=median)$.
+a$c = a$.
+
+ggplot(aes(x=log(c/t),y=n2,color=m), data=a[log(a$c/t)>-10,])+
+  #geom_density(aes(color=m),kernel="b",adjust=2)+
+  #geom_line(aes(group=interaction(m,n>0)),size=0.4)+
+  geom_point(alpha=0.5,size=0.5)+
+  theme_classic()+theme(legend.position = "None")+
+  facet_wrap(~interaction(r,v,sep=": "),scales="free",ncol=2)+ 
+  scale_color_brewer(name="",palette = "Dark2")+scale_fill_brewer(name="",palette = "Dark2")+
+  scale_y_log10(name="Penalty")+scale_x_continuous(name="Log likelihood (empirical)")
+
+ggsave("penalty_llh_normal.pdf",width=4,height = 4)
 
